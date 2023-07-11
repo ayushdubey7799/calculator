f24f63d81c9c15f57ce9aafa3608815535c8975e
Ayush Dubey <ayushdubey7799@gmail.com>
2023-07-11 16:26:54 +0530

    commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let display = document.getElementById("display");
+let buttons = document.querySelectorAll('.button');
+let stringToEvaluate = "";
+buttons.forEach(element => {
+    element.addEventListener('click',(e) => evaluate(e));
+});
+
+const evaluate = (e) => {
+    let x = e.target.innerHTML;
+    console.log(x);
+    if(x=="="){
+        stringToEvaluate = "" + eval(stringToEvaluate)
+
+    }
+    else if(x=="DEL"){
+        stringToEvaluate = stringToEvaluate.slice(0,-1);
+    }
+    else if(x=="RESET"){
+        stringToEvaluate = "";
+    }
+    else if(x=="X"){
+        x = "*";
+        stringToEvaluate+=x;
+    }
+    else{
+        stringToEvaluate+=x;
+    }
+   displayString();
+}
+
+function displayString(){
+    display.innerHTML = stringToEvaluate;
+    console.log(stringToEvaluate);
+}
+
